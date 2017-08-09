62e501d2213d3bc40e023a8f81867b0d758cf2a1
ydkpython <1539276969@qq.com>
2017-08-09 18:54:39 +0800

    新建文件

--- Audit_project3/urls.py
+++ Audit_project3/urls.py
@@ -1,4 +1,4 @@
-"""githot URL Configuration
+"""Audit_project3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app01 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index.html$/', views.index),
 ]
--- Audit_project3/wsgi.py
+++ Audit_project3/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for githot project.
+WSGI config for Audit_project3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "githot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Audit_project3.settings")
 
 application = get_wsgi_application()
